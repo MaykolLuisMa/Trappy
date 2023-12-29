d315993c1b8813de5d03abb5bb75705e85bab742
Davier <daviersanbell@gmail.com>
2023-12-29 12:43:44 -0500

    begginings of Conn

--- trapy/trapy.py
+++ trapy/trapy.py
@@ -1,22 +1,19 @@
-class Conn:
-    pass
-
-
-class ConnException(Exception):
-    pass
-
+from conn import Conn
+import utils
 
 def listen(address: str) -> Conn:
-    pass
-
+    host, port = utils.parse_address(address)
+    conn = Conn()
+    conn.socket.bind(host, port)
+    return conn
 
 def accept(conn) -> Conn:
     pass
 
 
 def dial(address) -> Conn:
+    host, port = utils.parse_address(address)
     pass
-
 
 def send(conn: Conn, data: bytes) -> int:
     pass
