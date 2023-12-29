d1bffd1333e366c9379dcd4d10e3e38cda28262a
chavely15k <chavely15k@gmail.com>
2023-12-29 15:21:11 -0500

    chronometer

--- trapy/chronometer.py
+++ trapy/chronometer.py
@@ -2,39 +2,39 @@ import time
 
 class Chronometer:
     def __init__(self):
-        self.time_to_wait = 0
-        self.start_time = None
-        self.finished = None
+        self._time_to_wait = 0
+        self._start_time = None
+        self._finished = None
         
     def _refresh(self):
-        if (self.finished == None):
-            self.finished = True
+        if (self._finished == None):
+            self._finished = True
         
-        if (not self.finished and self.start_time != None):
-            self.finished = time.time - self.start_time >= self.time_to_wait
+        if (not self._finished and self._start_time != None):
+            self._finished = time.time - self._start_time >= self._time_to_wait
 
-        if (self.start_time != None and self.finished):
-            self.start_time = None
-            self.time_to_wait = 0
+        if (self._start_time != None and self._finished):
+            self._start_time = None
+            self._time_to_wait = 0
     
     def start(self, wait = 3):
         self._refresh()
-        if(self.start_time != None):
+        if(self._start_time != None):
             assert("This chronometer is already in use")
             return
-        self.time_to_wait = wait
-        self.start_time = time.time()
-        self.finished = False
+        self._time_to_wait = wait
+        self._start_time = time.time()
+        self._finished = False
         
     def time_left(self):
-        if (self.finished == None):
+        if (self._finished == None):
             return 0
         if (self.timeout()):
             return 0
-        return self.time_to_wait - (time.time() - self.start_time)
+        return self._time_to_wait - (time.time() - self._start_time)
     
     def timeout(self):
-        if (self.finished == None): 
+        if (self._finished == None): 
             return False
         self._refresh()
-        return self.finished
+        return self._finished
