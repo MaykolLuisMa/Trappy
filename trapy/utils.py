05a276b6f51d1f4393c8b21df27296aa9ab3694f
chavely15k <chavely15k@gmail.com>
2023-12-30 00:14:41 -0500

    adding checksum

--- trapy/utils.py
+++ trapy/utils.py
@@ -1,3 +1,5 @@
+from struct import pack, unpack
+
 def parse_address(address):
     host, port = address.split(':')
 
@@ -5,3 +7,18 @@ def parse_address(address):
         host = 'localhost'
 
     return host, int(port)
+
+def make_checksum(data):
+    length = len(data)
+    checksum = 0
+    for i in range(length//2):
+        checksum += unpack('H', data[(2*i):(i*2)+2])[0]
+        if (checksum >= 2**16): checksum -= 2**16
+    if (length%2): checksum += unpack('H', data[(2*i):(i*2)+1])[0]
+    if (checksum >= 2**16): checksum -= 2**16
+    checksum += 1
+    if (checksum >= 2**16): checksum -= 2**16
+    return pack('H', checksum)
+
+def corrupted(data, checksum):
+    return checksum == make_checksum(data)
